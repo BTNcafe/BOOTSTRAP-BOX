97fc5e74f7e87a0639bc3f1f07578348030ef3ae
BTNcafe <admin@btncafe.com>
2014-02-19 15:19:59 +0900

    for attribute 지원

--- BOOTSTRAP/BROWSER/DOM/LABEL.js
+++ BOOTSTRAP/BROWSER/DOM/LABEL.js
@@ -1,12 +1,26 @@
 BOOTSTRAP.LABEL = CLASS({
 
-    preset : function() {'use strict';
-        return DOM;
-    },
+	preset : function() {'use strict';
+		return DOM;
+	},
 
-    params : function() {'use strict';
-        return {
-            tag : 'label'
-        };
-    }
+	params : function() {'use strict';
+		return {
+			tag : 'label'
+		};
+	},
+
+	init : function(cls, inner, self, params) {'use strict';
+		//REQUIRED: params
+		//OPTIONAL: params.f
+
+		var
+		// f
+		f = params.f;
+
+		inner.setAttr({
+			name : 'for',
+			value : f
+		});
+	}
 });
