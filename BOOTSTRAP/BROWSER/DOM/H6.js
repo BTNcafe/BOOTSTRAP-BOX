e655bcce37194e36133878c4ff5bf0cae2292730
BTNcafe <admin@btncafe.com>
2014-02-19 18:19:21 +0900

    no message

---
+++ BOOTSTRAP/BROWSER/DOM/H6.js
@@ -0,0 +1,13 @@
+BOOTSTRAP.H6 = CLASS({
+
+	preset : function() {'use strict';
+		return H6;
+	},
+
+	init : function(cls, inner, self, params) {'use strict';
+		BOOTSTRAP.inject({
+			inner : inner,
+			params : params
+		});
+	}
+});
